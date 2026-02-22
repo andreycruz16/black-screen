d37c60f027647e604ba4b792d695cd6bbe626b9d
Mark Andrey <markandreydelacruz@ymail.com>
2026-02-22 11:53:16 +0800

    Add wakelock_plus dependency and configure iOS and macOS Podfiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await WakelockPlus.enable();
   runApp(const BlackScreenApp());
 }
 
