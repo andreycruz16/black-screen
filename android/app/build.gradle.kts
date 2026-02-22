790bc5ed7b2b46164bc0b00bb5494bcc2aca38d0
Mark Andrey <markandreydelacruz@ymail.com>
2026-02-22 12:05:51 +0800

    Update package namespace and application ID to match new branding

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.black_screen"
+    namespace = "com.ikitaiapps.blackscreen"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.black_screen"
+        applicationId = "com.ikitaiapps.blackscreen"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/ikitaiapps/blackscreen/MainActivity.kt
+++ android/app/src/main/kotlin/com/ikitaiapps/blackscreen/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.black_screen
+package com.ikitaiapps.blackscreen
 
 import io.flutter.embedding.android.FlutterActivity
 
